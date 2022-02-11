80f254dc5de8c3f8db436b20ffad1abd6fb640b5
sglmp1 <sglmp1@WRD8F2CA8CC7F1.id.add.cardiff.ac.uk>
2022-02-11 11:05:05 +0000

    Adds #include to QWiic Arduino library

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -24,6 +24,7 @@
 /* USER CODE BEGIN Includes */
 #include <stdbool.h>
 #include <string.h>
+#include "SparkFun_Qwiic_Scale_NAU7802_Arduino_library.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
